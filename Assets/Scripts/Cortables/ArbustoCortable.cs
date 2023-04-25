a0aac72bee8b9fc696ecb04be6f1f0512d6d5903
KimmiRecords <diego.katabian@davinci.edu.ar>
2023-04-25 02:57:01 -0300

    arbustos cortables

---
+++ Assets/Scripts/Cortables/ArbustoCortable.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArbustoCortable : MonoBehaviour, ICortable
+{
+    public void GetCut(float dmg)
+    {
+        print("cortaste este arbusto");
+        AudioManager.instance.PlayRandom("TijeraHit01", "TijeraHit02");
+        Destroy(gameObject);
+    }
+}
