f86b2ae604ca62e13893ef98b73885431ed63f06
KimmiRecords <diego.katabian@davinci.edu.ar>
2023-09-06 15:13:27 -0300

    mini cambios para buildear

--- Assets/Scripts/Managers/CursorManager.cs
+++ Assets/Scripts/Managers/CursorManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 
 public enum CursorType
