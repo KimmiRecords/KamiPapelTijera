c0022484b7750fe7dc925144b87a392080d78aeb
Valen <valentino11roman@gmail.com>
2023-11-23 06:13:50 -0300

    Molino bien hecho + viento VFX

---
+++ Assets/Scripts/RotatingObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObject : MonoBehaviour
+{
+    public float velocidadRotacion = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right * velocidadRotacion * Time.deltaTime);
+    }
+}
