474a400b5495a78bae363061d1ca4de405f69edc
Valen <valentino11roman@gmail.com>
2023-12-18 06:23:05 -0300

    cofre + pedestal particles

--- Assets/Scripts/TriggerS/TriggerOrigami.cs
+++ Assets/Scripts/TriggerS/TriggerOrigami.cs
@@ -16,24 +16,29 @@ public class TriggerOrigami : TriggerScript
     public Color blueParticleActiveColor;
     public float activeSpeed = 1.5f;
     public float activeSize = 2f;
+    public float orbitalZ = 2f;
 
     //auxiliares
     MultipleRectCheck currentCheck;
     ParticleSystem ps;
     ParticleSystem.MainModule mainModule;
+    ParticleSystem.VelocityOverLifetimeModule velocityModule;
     Color originalStartColor;
     float originalStartSpeed;
     float originalStartSize;
+    float originalorbitalZ;
 
     protected override void Start()
     {
         base.Start();
         ps = particleSystemGO.GetComponent<ParticleSystem>();
         mainModule = ps.main;
+        velocityModule = ps.velocityOverLifetime;
 
         originalStartColor = mainModule.startColor.color;
         originalStartSpeed = mainModule.startSpeed.constant;
         originalStartSize = mainModule.startSize.constant;
+        originalorbitalZ = velocityModule.orbitalZ.constant;
         //originalEmissionRate = mainModule.emission.rateOverTime.constant;
     }
 
@@ -79,6 +84,7 @@ public class TriggerOrigami : TriggerScript
         mainModule.startColor = blueParticleActiveColor;
         mainModule.startSpeed = originalStartSpeed * activeSpeed;
         mainModule.startSize = activeSize;
+        velocityModule.orbitalZ = orbitalZ;
         //mainModule.emission.rateOverTime = originalEmissionRate * 2f;
     }
 
@@ -87,6 +93,7 @@ public class TriggerOrigami : TriggerScript
         mainModule.startColor = originalStartColor;
         mainModule.startSpeed = originalStartSpeed;
         mainModule.startSize = originalStartSize;
+        velocityModule.orbitalZ = originalorbitalZ;
         //mainModule.emission.rateOverTime = originalEmissionRate;
     }
 }
